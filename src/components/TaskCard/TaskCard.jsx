3afc402d065ea771902b4cc6def4c07973651d87
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-20 17:42:33 +0600

    TaskCard describes the details of a todo task

---
+++ src/components/TaskCard/TaskCard.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import "./TaskCard.scss";
+
+function TaskCard({ title, createdAt }) {
+  return (
+    <div className="grid-container__box">
+      <h3>{title}</h3>
+      <p>Created At: {createdAt}</p>
+    </div>
+  );
+}
+
+TaskCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired,
+};
+
+export default TaskCard;
