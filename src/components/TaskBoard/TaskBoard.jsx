057d0e4ec1663c6d30f5260b924944f6a3eabd29
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-21 15:53:10 +0600

    1. Collection of tasks is not mapped in this component anymore, for that purpose a new component TaskList is created and included 2. Add AddTaskComponent for adding new tasks, addTask action is dispatched from where to store the new task in redux store state named tasks 3. handleCreate button prevents consecutive new tasks to be added, in case the first task created is not dealt with yet.

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -1,22 +1,36 @@
-import { useSelector } from "react-redux";
-import TaskCard from "../TaskCard/TaskCard";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import TaskNavBar from "../TaskNavBar/TaskNavBar";
+import AddTaskCard from "../AddTaskCard/AddTaskCard";
+import { addTask } from "../../store/todo/actions/todoActions";
+import TaskList from "../TaskList/TaskList";
 import "./TaskBoard.scss";
 
 function TaskBoard() {
+  const [isCreateButtonClicked, setIsCreateButtonClicked] = useState(false);
+
   const tasks = useSelector((state) => state.todo.tasks);
+  const dispatch = useDispatch();
+
+  function handleCreateButton() {
+    setIsCreateButtonClicked(!isCreateButtonClicked);
+  }
+
+  function handleCreateTask(taskTitle) {
+    handleCreateButton();
+    dispatch(addTask({ taskTitle: taskTitle }));
+  }
 
   return (
     <>
-      <TaskNavBar />
+      <TaskNavBar onCreateButtonClicked={handleCreateButton} />
+
       <div className="grid-container">
-        {tasks.map((task) => (
-          <TaskCard
-            title={task.title}
-            createdAt={task.createdAt}
-            key={task.id}
-          />
-        ))}
+        {isCreateButtonClicked && (
+          <AddTaskCard onCreateTask={handleCreateTask} />
+        )}
+
+        <TaskList tasks={tasks} />
       </div>
     </>
   );
