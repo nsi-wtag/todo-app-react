6dd8d2173fa9980064c7f9e3adac06b7b23cb2cc
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-21 15:37:30 +0600

    1. Add Current Date as id of the task by default 2. Add createdAt on the fly using current date, format the date into dd.mm.yyyy format

--- src/store/todo/actions/todoActions.js
+++ src/store/todo/actions/todoActions.js
@@ -1,8 +1,14 @@
 import { todoActionTypes } from "../types/todoTypes";
+import { formatDate } from "../../../utils/formatDate";
 
 const { ADD_TASK } = todoActionTypes;
 
-export const addTask = (task) => ({
-  type: ADD_TASK,
-  payload: task,
-});
+export const addTask = (task) => {
+  task.id = Date.now().toString();
+  task.createdAt = formatDate(new Date());
+
+  return {
+    type: ADD_TASK,
+    payload: task,
+  };
+};
