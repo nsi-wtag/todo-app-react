90dd66474752eff9cb58d456d876a4a2d0fd00d8
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-21 18:31:50 +0600

    adjust the imports and variables name to sync with the latest filenames, as todo was replaced with task in previous commit

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TaskNavBar from "../TaskNavBar/TaskNavBar";
 import AddTaskCard from "../AddTaskCard/AddTaskCard";
-import { addTask } from "../../store/todo/actions/todoActions";
+import { addTask } from "../../store/task/actions/taskActions";
 import TaskList from "../TaskList/TaskList";
 import "./TaskBoard.scss";
 
 function TaskBoard() {
   const [isCreateButtonClicked, setIsCreateButtonClicked] = useState(false);
 
-  const tasks = useSelector((state) => state.todo.tasks);
+  const tasks = useSelector((state) => state.task.tasks);
   const dispatch = useDispatch();
 
   function handleCreateButton() {
