283966794e6ca1b8ceac3ec1489c0e59caaef846
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-22 15:01:27 +0600

    refactor: reorder the imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
