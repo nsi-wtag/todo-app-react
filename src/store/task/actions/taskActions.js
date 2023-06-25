8a2a8f42e8e818e2f5c7ce959853731f11b095e0
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-25 16:07:48 +0600

    add new action markTaskDone

--- src/store/task/actions/taskActions.js
+++ src/store/task/actions/taskActions.js
@@ -1,11 +1,12 @@
 import { taskActionTypes } from "src/store/task/types/taskTypes";
 import { formatDate } from "src/utils/formatDate";
 
-const { ADD_TASK, DELETE_TASK } = taskActionTypes;
+const { ADD_TASK, DELETE_TASK, DONE_TASK } = taskActionTypes;
 
 export const addTask = (task) => {
   task.taskId = Date.now().toString();
   task.createdAt = formatDate(new Date());
+  task.isTaskDone = false;
 
   return {
     type: ADD_TASK,
@@ -16,6 +17,13 @@ export const addTask = (task) => {
 export const deleteTask = (taskId) => {
   return {
     type: DELETE_TASK,
+    payload: taskId,
+  };
+};
+
+export const markTaskDone = (taskId) => {
+  return {
+    type: DONE_TASK,
     payload: taskId,
   };
 };
