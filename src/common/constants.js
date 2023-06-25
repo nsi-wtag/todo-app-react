2ee83f668cccc2873af9cb9891797b97e5e49907
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-25 11:17:06 +0600

    add delete icon and alt text for delete icon as constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,1 +1,6 @@
+import deleteIcon from "src/assets/icons/deleteIcon.png";
+
+export const ICON_DELETE = deleteIcon;
+
 export const PLACEHOLDER_SEARCH_TASK = "Search...";
+export const ALT_TEXT_DELETE_ICON = "delete icon";
