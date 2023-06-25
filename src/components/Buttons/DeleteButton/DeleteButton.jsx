ed878f73cb5a5a476f16005e57d7c8a8cdc9cf68
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-25 17:02:28 +0600

    refactor: icon buttons now have a common styling file buttons.scss, thus we can remove DeleteButton.scss

--- src/components/Buttons/DeleteButton/DeleteButton.jsx
+++ src/components/Buttons/DeleteButton/DeleteButton.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import { ALT_TEXT_DELETE_ICON, ICON_DELETE } from "src/common/constants";
-import "./DeleteButton.scss";
+import "src/components/Buttons/buttons.scss";
 
 function DeleteButton({ onDelete }) {
   return (
-    <button onClick={onDelete} className="button-delete">
+    <button onClick={onDelete} className="button-icon">
       <img src={ICON_DELETE} alt={ALT_TEXT_DELETE_ICON} />
     </button>
   );
