dd4b7f7c9ec22947519808b0b83d62590379732d
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-25 16:43:35 +0600

    add doneIcon as ICON_DONE constant, also add alt text for done icon

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,9 @@
 import deleteIcon from "src/assets/icons/deleteIcon.png";
+import doneIcon from "src/assets/icons/doneIcon.png";
 
 export const ICON_DELETE = deleteIcon;
+export const ICON_DONE = doneIcon;
 
 export const PLACEHOLDER_SEARCH_TASK = "Search...";
 export const ALT_TEXT_DELETE_ICON = "delete icon";
+export const ALT_TEXT_DONE_ICON = "done icon";
