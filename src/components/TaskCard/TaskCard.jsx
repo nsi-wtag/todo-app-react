c76e544d5c43d7b73d4a41aa09f553ef794f33e1
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-25 10:24:37 +0600

    add a delete task button, and dispatch deleteTask action on button click

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,16 +1,26 @@
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
+import { deleteTask } from "src/store/task/actions/taskActions";
 import "./TaskCard.scss";
 
-function TaskCard({ taskTitle, createdAt }) {
+function TaskCard({ taskId, taskTitle, createdAt }) {
+  const dispatch = useDispatch();
+
+  function handleDeleteTask() {
+    dispatch(deleteTask(taskId));
+  }
+
   return (
     <div className="tasks-container__box">
       <h3>{taskTitle}</h3>
       <p>Created At: {createdAt}</p>
+      <button onClick={handleDeleteTask}>Delete Task</button>
     </div>
   );
 }
 
 TaskCard.propTypes = {
+  taskId: PropTypes.string.isRequired,
   taskTitle: PropTypes.string.isRequired,
   createdAt: PropTypes.string.isRequired,
 };
