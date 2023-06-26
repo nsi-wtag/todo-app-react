de5bbaa31b141822668ade8fbe6c40c5dfc35238
Naimul Islam Siam <naimul_siam@Naimuls-Mac-mini.local>
2023-06-26 11:23:56 +0600

    add new constatns for input and button types

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,6 @@
 export const PLACEHOLDER_SEARCH_TASK = "Search...";
 
 export const FILTER_TASKS_BUTTON_TEXT_ARRAY = ["All", "Incomplete", "Complete"];
+
+export const TYPE_SUBMIT = "submit";
+export const TYPE_TEXT = "text";
