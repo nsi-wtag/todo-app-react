2cf98277fd643cbaa2ba95211e6ca32bb1b1e20e
nsi-wtag <naimul.siam@welldev.io>
2023-07-02 18:12:12 +0600

    add and validate onEdit prop

--- src/components/Buttons/EditButton/EditButton.jsx
+++ src/components/Buttons/EditButton/EditButton.jsx
@@ -1,12 +1,17 @@
+import PropTypes from "prop-types";
 import { ICON_EDIT, ALT_TEXT_EDIT_ICON } from "src/common/constants";
 import "src/components/Buttons/buttons.scss";
 
-function EditButton() {
+function EditButton({ onEdit }) {
   return (
-    <button className="button-icon">
+    <button className="button-icon" onClick={onEdit}>
       <img src={ICON_EDIT} alt={ALT_TEXT_EDIT_ICON} />
     </button>
   );
 }
+
+EditButton.propTypes = {
+  onEdit: PropTypes.func.isRequired,
+};
 
 export default EditButton;
