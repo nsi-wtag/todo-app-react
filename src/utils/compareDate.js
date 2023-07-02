b0cc82901f6083440512577da133b7970b4dcff8
nsi-wtag <naimul.siam@welldev.io>
2023-07-02 12:34:37 +0600

    refactor: use the constant instead of using magic values directly

--- src/utils/compareDate.js
+++ src/utils/compareDate.js
@@ -1,5 +1,7 @@
+import { MILLI_SECONDS_IN_A_DAY } from "src/common/constants";
+
 export function getDaysToCompleteTask(startDate, endDate) {
-  const millisecondsInADay = 24 * 60 * 60 * 1000;
+  const millisecondsInADay = MILLI_SECONDS_IN_A_DAY;
   const diffInMilliseconds = Math.abs(endDate - startDate);
   const diffInDays = Math.round(diffInMilliseconds / millisecondsInADay) + 1;
 
