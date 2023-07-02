79a1e8684df0a0caa26e2f16524c2eb6c371e54c
nsi-wtag <naimul.siam@welldev.io>
2023-07-02 18:14:24 +0600

    major refactor: TaskCard component (previous) is now TaskCardPreview component - which shows the default view of a task in card

---
+++ src/components/TaskCardPreview/TaskCardPreview.jsx
@@ -0,0 +1,62 @@
+import { useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+import { deleteTask, markTaskDone } from "src/store/task/actions/taskActions";
+import { formatDate } from "src/utils/formatDate";
+import TaskCardButtonsContainer from "src/components/TaskCardButtonsContainer/TaskCardButtonsContainer";
+import "./TaskCardPreview.scss";
+
+function TaskCardPreview({
+  taskId,
+  taskTitle,
+  createdAt,
+  taskDoneAt,
+  isTaskDone,
+  onToggleEditTask,
+}) {
+  const dispatch = useDispatch();
+
+  function handleDeleteTask() {
+    dispatch(deleteTask(taskId));
+  }
+
+  function handleTaskDone() {
+    dispatch(markTaskDone(taskId));
+  }
+
+  function handleEditTask() {
+    onToggleEditTask();
+  }
+
+  return (
+    <>
+      <h3 className={`${isTaskDone && "task_done"}`}>{taskTitle}</h3>
+      <p>Created At: {formatDate(createdAt)}</p>
+
+      <div className="tasks-container__box-buttons">
+        <TaskCardButtonsContainer
+          isTaskDone={isTaskDone}
+          createdAt={createdAt}
+          taskDoneAt={taskDoneAt}
+          onDeleteTask={handleDeleteTask}
+          onTaskDone={handleTaskDone}
+          onEditTask={handleEditTask}
+        />
+      </div>
+    </>
+  );
+}
+
+TaskCardPreview.defaultProps = {
+  taskDoneAt: null,
+};
+
+TaskCardPreview.propTypes = {
+  taskId: PropTypes.string.isRequired,
+  taskTitle: PropTypes.string.isRequired,
+  createdAt: PropTypes.instanceOf(Date).isRequired,
+  taskDoneAt: PropTypes.instanceOf(Date),
+  isTaskDone: PropTypes.bool.isRequired,
+  onToggleEditTask: PropTypes.func.isRequired,
+};
+
+export default TaskCardPreview;
