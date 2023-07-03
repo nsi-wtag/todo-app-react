1973725d2681705649e94279718d04f59f7b596a
nsi-wtag <naimul.siam@welldev.io>
2023-07-03 11:30:45 +0600

    1. input text is sanitized 2. save button is enabled or disabled based on the isSaveButtonDisabled variable 3. editTask action is dispatched on save button click, if the taskTitle is edited

--- src/components/EditTaskCard/EditTaskCard.jsx
+++ src/components/EditTaskCard/EditTaskCard.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
+import { editTask } from "src/store/task/actions/taskActions";
+import { sanitizeText } from "src/utils/sanitizeText";
 import DeleteButton from "src/components/Buttons/DeleteButton/DeleteButton";
 import "src/components/AddTaskCard/AddTaskCard.scss";
 
-function EditTaskCard({ taskTitle, onToggleEditTask }) {
+function EditTaskCard({ taskId, taskTitle, onToggleEditTask }) {
   const [inputText, setInputText] = useState(taskTitle);
+  const sanitizedInput = sanitizeText(inputText);
+  const isSaveButtonDisabled = sanitizeText(inputText) === "";
+  const dispatch = useDispatch();
 
   function handleChange(event) {
     setInputText(event.target.value);
   }
 
   function handleSave() {
+    if (sanitizedInput !== taskTitle) {
+      dispatch(editTask({ taskId, taskTitle: sanitizedInput }));
+    }
+
     onToggleEditTask();
   }
 
@@ -28,7 +38,15 @@ function EditTaskCard({ taskTitle, onToggleEditTask }) {
       />
 
       <div className="tasks-container__box-buttons">
-        <button onClick={handleSave}>Save</button>
+        <button
+          className={`task__save-button ${
+            isSaveButtonDisabled ? "task__save-button-disabled " : null
+          }`}
+          disabled={isSaveButtonDisabled}
+          onClick={handleSave}
+        >
+          Save
+        </button>
         <DeleteButton onDelete={handleDelete} />
       </div>
     </>
@@ -36,6 +54,7 @@ function EditTaskCard({ taskTitle, onToggleEditTask }) {
 }
 
 EditTaskCard.propTypes = {
+  taskId: PropTypes.string.isRequired,
   taskTitle: PropTypes.string.isRequired,
   onToggleEditTask: PropTypes.func.isRequired,
 };
