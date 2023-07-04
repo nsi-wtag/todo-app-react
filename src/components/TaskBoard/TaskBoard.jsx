50b63c735d7d2f3dd45c0db3b877d75e15daa093
nsi-wtag <naimul.siam@welldev.io>
2023-07-04 18:08:17 +0600

    add pagination and display different set of task cards based on the loading button and number of tasks

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTask } from "src/store/task/actions/taskActions";
+import { MAX_TASK_PER_PAGE } from "src/common/constants";
 import TaskNavBar from "src/components/TaskNavBar/TaskNavBar";
 import AddTaskCard from "src/components/AddTaskCard/AddTaskCard";
 import TaskList from "src/components/TaskList/TaskList";
@@ -8,9 +9,31 @@ import "./TaskBoard.scss";
 
 function TaskBoard() {
   const [isCreateButtonClicked, setIsCreateButtonClicked] = useState(false);
-
+  const [visibleTaskRange, setVisibleTaskRange] = useState(MAX_TASK_PER_PAGE);
   const tasks = useSelector((state) => state.task.tasks);
   const dispatch = useDispatch();
+
+  const totalTasks = tasks.length;
+  let numberOfTasksOnScreen = totalTasks;
+
+  if (isCreateButtonClicked) {
+    numberOfTasksOnScreen += 1;
+  }
+
+  const isLoadMore =
+    numberOfTasksOnScreen > MAX_TASK_PER_PAGE && visibleTaskRange <= totalTasks;
+  const isLoadLess =
+    visibleTaskRange >= totalTasks && totalTasks > MAX_TASK_PER_PAGE;
+
+  console.log(numberOfTasksOnScreen);
+
+  const showMoreItems = () => {
+    setVisibleTaskRange((prevValue) => prevValue + MAX_TASK_PER_PAGE);
+  };
+
+  function showLessItems() {
+    setVisibleTaskRange(MAX_TASK_PER_PAGE);
+  }
 
   function handleCreateButton() {
     setIsCreateButtonClicked(!isCreateButtonClicked);
@@ -21,8 +44,15 @@ function TaskBoard() {
     dispatch(addTask({ taskTitle: taskTitle }));
   }
 
+  useEffect(() => {
+    if (numberOfTasksOnScreen <= MAX_TASK_PER_PAGE) {
+      showLessItems();
+      console.log("Triggered");
+    }
+  }, [numberOfTasksOnScreen]);
+
   return (
-    <>
+    <div className="tasks-board-container">
       <TaskNavBar onCreateButtonClick={handleCreateButton} />
 
       <div className="tasks-container">
@@ -30,9 +60,32 @@ function TaskBoard() {
           <AddTaskCard onCreateTask={handleCreateTask} />
         )}
 
-        <TaskList tasks={tasks} />
+        <TaskList
+          tasks={tasks}
+          visibleTaskRange={visibleTaskRange}
+          isCreateButtonClicked={isCreateButtonClicked}
+        />
       </div>
-    </>
+
+      <div className="tasks-board-container__buttons-container">
+        {isLoadMore && (
+          <button
+            className="tasks-board-container__load-button"
+            onClick={showMoreItems}
+          >
+            Load More
+          </button>
+        )}
+        {isLoadLess && (
+          <button
+            className="tasks-board-container__load-button"
+            onClick={showLessItems}
+          >
+            Load Less
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
 
