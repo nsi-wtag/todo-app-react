536dafad60845549c8a07f5583cf6c313f9d0f87
nsi-wtag <naimul.siam@welldev.io>
2023-07-04 18:06:31 +0600

    slice the tasks array according to the pagination range, for displaying task cards based on the load and pagination status

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,7 +1,34 @@
+import PropTypes from "prop-types";
 import TaskCard from "src/components/TaskCard/TaskCard";
 
-function TaskList({ tasks }) {
-  return tasks.map((task) => <TaskCard task={task} key={task.taskId} />);
+function TaskList({ tasks, visibleTaskRange, isCreateButtonClicked }) {
+  let rangeOfTasks = visibleTaskRange;
+
+  if (isCreateButtonClicked) {
+    rangeOfTasks -= 1;
+  }
+
+  return (
+    <>
+      {tasks?.slice(0, rangeOfTasks).map((task) => (
+        <TaskCard task={task} key={task.taskId} />
+      ))}
+    </>
+  );
 }
+
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      taskId: PropTypes.string.isRequired,
+      taskTitle: PropTypes.string.isRequired,
+      createdAt: PropTypes.instanceOf(Date).isRequired,
+      taskDoneAt: PropTypes.instanceOf(Date),
+      isTaskDone: PropTypes.bool.isRequired,
+    })
+  ),
+  visibleTaskRange: PropTypes.number.isRequired,
+  isCreateButtonClicked: PropTypes.bool.isRequired,
+};
 
 export default TaskList;
