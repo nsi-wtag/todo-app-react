03bdd385d9df5f6e07c004ee9514d16b217d004b
nsi-wtag <naimul.siam@welldev.io>
2023-07-05 14:28:24 +0600

    add isDisabled prop

--- src/components/Buttons/IconButton/IconButton.jsx
+++ src/components/Buttons/IconButton/IconButton.jsx
@@ -1,18 +1,27 @@
 import PropTypes from "prop-types";
 import "src/components/Buttons/buttons.scss";
 
-function IconButton({ buttonSrc, buttonAltText, onAction }) {
+function IconButton({ buttonSrc, buttonAltText, onAction, isDisabled }) {
   return (
-    <button onClick={onAction} className="button-icon">
+    <button
+      onClick={onAction}
+      className={`button-icon ${isDisabled && "button-icon-disabled"}`}
+      disabled={isDisabled}
+    >
       <img src={buttonSrc} alt={buttonAltText} />
     </button>
   );
 }
 
+IconButton.defaultProps = {
+  isDisabled: false,
+};
+
 IconButton.propTypes = {
   buttonSrc: PropTypes.string.isRequired,
   buttonAltText: PropTypes.string.isRequired,
   onAction: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool,
 };
 
 export default IconButton;
