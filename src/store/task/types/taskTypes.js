733c13b78d11c33fe7eecd0930477e25cbcca897
nsi-wtag <naimul.siam@welldev.io>
2023-07-09 11:23:50 +0600

    refactor: instead of grouping the action types under a taskActionTypes object, create and export separate const variables for each task action type

--- src/store/task/types/taskTypes.js
+++ src/store/task/types/taskTypes.js
@@ -1,6 +1,4 @@
-export const taskActionTypes = {
-  ADD_TASK: "ADD_TASK",
-  DELETE_TASK: "DELETE_TASK",
-  DONE_TASK: "DONE_TASK",
-  EDIT_TASK: "EDIT_TASK",
-};
+export const ADD_TASK = "ADD_TASK";
+export const DELETE_TASK = "DELETE_TASK";
+export const DONE_TASK = "DONE_TASK";
+export const EDIT_TASK = "EDIT_TASK";
