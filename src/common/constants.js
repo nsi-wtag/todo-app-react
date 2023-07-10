fd5e99a40d679ec6a3a7e4d42e0708981671638f
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-10 10:57:08 +0600

    refactor: capitalize the delete icon alt text

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,7 +3,7 @@ import deleteIcon from "src/assets/icons/deleteIcon.png";
 export const ICON_DELETE = deleteIcon;
 
 export const PLACEHOLDER_SEARCH_TASK = "Search...";
-export const ALT_TEXT_DELETE_ICON = "delete icon";
+export const ALT_TEXT_DELETE_ICON = "Delete Icon";
 
 export const FILTER_TASKS_BUTTON_TEXT_ARRAY = ["All", "Incomplete", "Complete"];
 
