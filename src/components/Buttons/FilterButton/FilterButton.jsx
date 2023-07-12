f2d1e65eabecfa76aa8d47b2c769c502adc19c62
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-12 16:31:23 +0600

    pass and validate a new prop named onClick

--- src/components/Buttons/FilterButton/FilterButton.jsx
+++ src/components/Buttons/FilterButton/FilterButton.jsx
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 import "src/components/TaskNavBar/TaskNavBar.scss";
 
-function FilterButton({ buttonText }) {
-  return <button className="task-navbar__filter">{buttonText}</button>;
+function FilterButton({ buttonText, onClick }) {
+  return (
+    <button className="task-navbar__filter" onClick={onClick}>
+      {buttonText}
+    </button>
+  );
 }
 
 FilterButton.propTypes = {
   buttonText: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default FilterButton;
