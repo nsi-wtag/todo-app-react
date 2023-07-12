860a573a33ba3a769d9ff4a807449b6160040bd3
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-12 16:38:20 +0600

    refactor: for consistency use constant declared in common, instead of manually writing each string

--- src/store/filter/types/filterTypes.js
+++ src/store/filter/types/filterTypes.js
@@ -1,4 +1,6 @@
-export const ALL = "ALL";
-export const INCOMPLETE = "Incomplete";
-export const COMPLETE = "Complete";
+import { FILTER_TASKS_BUTTON_LABELS } from "src/common/constants";
+
+export const ALL = FILTER_TASKS_BUTTON_LABELS[0];
+export const INCOMPLETE = FILTER_TASKS_BUTTON_LABELS[1];
+export const COMPLETE = FILTER_TASKS_BUTTON_LABELS[2];
 export const SET_FILTER = "SET_FILTER";
