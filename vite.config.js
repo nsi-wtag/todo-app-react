62cd0e15fe6015f7f3fa0fa161d4a9c143b7c066
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-16 16:54:20 +0600

    config vite.config.js file for setting up absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: "/src",
+      "@": path.resolve(__dirname, "./src/"),
+      "@assets": `${path.resolve(__dirname, "./src/assets/")}`,
+      "@common": `${path.resolve(__dirname, "./src/common/")}`,
+      "@components": `${path.resolve(__dirname, "./src/components/")}`,
+      "@store": `${path.resolve(__dirname, "./src/store/")}`,
+      "@styles": `${path.resolve(__dirname, "./src/styles/")}`,
+      "@utils": `${path.resolve(__dirname, "./src/utils/")}`,
     },
   },
   css: {
