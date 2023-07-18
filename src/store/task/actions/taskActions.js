183113f5d0d68035cc83c26942f19baf7646a8ff
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-18 10:04:40 +0600

    refactor: store date as an instance of Date, instead of storing as string

--- src/store/task/actions/taskActions.js
+++ src/store/task/actions/taskActions.js
@@ -1,9 +1,8 @@
 import { ADD_TASK } from "@store/task/types/taskTypes";
-import { formatDate } from "@/utils/formatDate";
 
 export const addTask = (task) => {
   task.id = Date.now().toString();
-  task.createdAt = formatDate(new Date());
+  task.createdAt = new Date();
 
   return {
     type: ADD_TASK,
