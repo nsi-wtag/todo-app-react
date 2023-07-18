cb55da61d17fca3adc411a03c2f6dabe036c2f72
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-18 10:05:25 +0600

    refactor: as date is now stored as date, format that and change the prop type to an instance of Date

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,18 +1,19 @@
 import PropTypes from "prop-types";
+import { formatDate } from "@utils/formatDate";
 import "./TaskCard.scss";
 
 function TaskCard({ title, createdAt }) {
   return (
     <div className="tasks-container__box">
       <h3>{title}</h3>
-      <p>Created At: {createdAt}</p>
+      <p>Created At: {formatDate(createdAt)}</p>
     </div>
   );
 }
 
 TaskCard.propTypes = {
   title: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired,
+  createdAt: PropTypes.instanceOf(Date).isRequired,
 };
 
 export default TaskCard;
