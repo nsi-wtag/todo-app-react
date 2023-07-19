93cfd60c83aca2c7543631dd31e937e39ecdc142
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-19 15:20:37 +0600

    include react-toastify ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { ToastContainer, Flip } from "react-toastify";
 import Header from "src/components/Header/Header";
 import TaskBoard from "src/components/TaskBoard/TaskBoard";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
+      <ToastContainer transition={Flip} />
       <Header />
       <TaskBoard />
     </>
