b413cbcd6b41bdeac3f2d0d977ad0a0c287fa7f8
Naimul Islam Siam <naimul.siam@welldev.io>
2023-07-27 17:22:04 +0600

    add new test for FilterButton to check if it is getting the text content properly

--- src/components/Buttons/FilterButton/FilterButton.test.jsx
+++ src/components/Buttons/FilterButton/FilterButton.test.jsx
@@ -4,22 +4,24 @@ import userEvent from "@testing-library/user-event";
 import FilterButton from "./FilterButton";
 
 describe("FilterButton", () => {
+  const user = userEvent.setup();
+  const onClickMock = vi.fn();
+
   it("Should render FilterButton component provided with a button text", () => {
     const buttonText = "Button Text";
     const { getByText } = render(<FilterButton buttonText={buttonText} />);
     const filterButtonElement = getByText(buttonText);
 
     expect(filterButtonElement).toBeInTheDocument();
+    expect(filterButtonElement.textContent).toBe(buttonText);
   });
 
   it("Should call the onClick function, when clicked", async () => {
     const buttonText = "Testing on click";
-    const onClickMock = vi.fn();
     const { getByText } = render(
       <FilterButton buttonText={buttonText} onClick={onClickMock} />
     );
     const buttonElement = getByText(buttonText);
-    const user = userEvent.setup();
 
     await user.click(buttonElement);
     expect(onClickMock).toHaveBeenCalledTimes(1);
